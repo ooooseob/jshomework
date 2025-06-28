0824436d421fb0876375065cdf790f67abe8ee1d
oooseob <sws4701@naver.com>
2025-06-29 08:42:39 +0900

    [3주차 과제]
    3.직접 만든 디자인 → 개발
    
    1. 마크업 수정
    2. js코드 작성
    (array.from, forEach, classList.toggle)

---
+++ src/03-homework/03-homework.js
@@ -0,0 +1,23 @@
+// const container = document.querySelector(".accordion-container");
+// const header = container.querySelector(".accordion-header .btn");
+// const content = container.querySelector(".accordion-content");
+
+// console.log(header);
+// console.log(content);
+
+// header.addEventListener("click", () => {
+//   container.classList.toggle("is-open");
+//   content.classList.toggle("is-open");
+// });
+
+const containers = Array.from(document.querySelectorAll(".accordion-container"));
+
+
+containers.forEach((accordion) => {
+  const button = accordion.querySelector(".accordion-header .btn");
+  
+  button.addEventListener("click", () => {
+    accordion.classList.toggle("is-open");
+  });
+
+});
